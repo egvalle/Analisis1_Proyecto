0102c909896b25764474f3203cbaff3449fc11c0
Edwin <evalleb@miumg.edu.gt>
2026-05-15 00:37:37 -0600

    Configuración inicial de frontend y backend

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-green-700">
+        Cosecha Red
+      </h1>
+    </div>
+  )
+}
