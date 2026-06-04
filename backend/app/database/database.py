4d37fadede094fd38cad54e99935e3b2663b71e1
Edwin <evalleb@miumg.edu.gt>
2026-06-03 22:23:46 -0600

    Mejorar configuración de base de datos

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -2,36 +2,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from dotenv import load_dotenv
-
 import os
 
-load_dotenv()
+# Obtener la URL de la base de datos
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL no está configurada")
 
-DATABASE_URL = (
-    "postgresql+psycopg2://"
-    f"{DB_USER}:{DB_PASSWORD}"
-    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
-
+# Crear el engine
 engine = create_engine(
     DATABASE_URL,
-    echo=True,
-    pool_pre_ping=True
+    pool_pre_ping=True,
+    connect_args={"sslmode": "require"}
 )
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
