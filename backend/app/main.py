90aab9963f5d0e9fa0dca5f9066d75820231bafa
Edwin <evalleb@miumg.edu.gt>
2026-06-03 23:43:10 -0600

    Agregar dominios de Vercel a CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,11 +12,12 @@ app.add_middleware(
         "http://localhost:5173",                                         # Desarrollo local
         "https://cosecha-red.vercel.app",                                # Tu dominio principal de Vercel
         "https://cosecha-7pux9vbvz-edwin-valle-s-projects.vercel.app",  # El dominio del error
-        "https://cosecha-git-develop-edwin-valle-s-projects.vercel.app",
+        "https://cosecha-git-main-edwin-valle-s-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
+    allow_origins_regex=r"https://.*\.vercel\.app"
 )
 
 """ ROUTES """
